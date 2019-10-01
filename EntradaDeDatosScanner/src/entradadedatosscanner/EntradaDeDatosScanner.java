3211a9a7404fb13695f6d5b306f288aebd07d72c
Diego Diaz <diego.diaz@desarrollo.ddbcol.com.co>
2019-10-01 15:13:42 -0500

    Ejercicio de captura de datos, mediante la libreria Scanner, y suma de numeros capturados

---
+++ EntradaDeDatosScanner/src/entradadedatosscanner/EntradaDeDatosScanner.java
@@ -0,0 +1,35 @@
+package entradadedatosscanner;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author diego.diaz
+ */
+public class EntradaDeDatosScanner {
+
+    public static void main(String[] args) {
+        
+        String nombre;
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("ingresa tu nombre");
+        nombre = teclado.next();
+        
+        System.out.println("Ingrese por favor dos numeros para sumarlos");
+        System.out.println("ingresa tu nombre");
+        a = teclado.nextInt();
+      
+        System.out.println("ingresa tu nombre");
+        b = teclado.nextInt();
+        
+        c = a + b;
+        
+        System.out.println(nombre + " El resultado de la suma es : " + c );
+
+    }
+    
+}
