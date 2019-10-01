9736381d9984c7c580da63220fa87361925357b9
Diego Diaz <diego.diaz@desarrollo.ddbcol.com.co>
2019-10-01 15:03:03 -0500

    Ejercicio suma de dos numeros, utilizando variables

---
+++ 1-Suma/src/pkg1/suma/Suma.java
@@ -0,0 +1,17 @@
+package pkg1.suma;
+
+/**
+ *
+ * @author diego.diaz
+ */
+public class Suma {
+
+    public static void main(String[] args) {
+        int numero1 = 30;
+        int numero2 = 56;
+        int r = numero1 + numero2;
+        System.out.println("La suma de los numeros es: " + r);
+
+    }
+    
+}
