84e624ae03093c0fe4954c85a14201a17aae1e38
Diego Diaz <diego.diaz@desarrollo.ddbcol.com.co>
2019-10-01 15:06:27 -0500

    Ejercicio divicion, utilizando variables y operadores

---
+++ 2-DivisionOperadores/src/pkg2/divisionoperadores/DivisionOperadores.java
@@ -0,0 +1,17 @@
+package pkg2.divisionoperadores;
+
+/**
+ *
+ * @author diego.diaz
+ */
+public class DivisionOperadores {
+
+    public static void main(String[] args) {
+        int numero1 = 5;
+        int numero2 = 7;
+        int rd = (numero1 + numero2) / 2;
+        System.out.println("La division es: " + rd);
+
+    }
+    
+}
