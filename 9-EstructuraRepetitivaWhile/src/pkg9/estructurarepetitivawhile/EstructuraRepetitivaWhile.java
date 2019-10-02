7e9dd202c234d19b6710099563324a7f39a5591b
Diego Diaz <diego.diaz@desarrollo.ddbcol.com.co>
2019-10-02 07:49:57 -0500

    Ejercicio estructura repetitiva while

---
+++ 9-EstructuraRepetitivaWhile/src/pkg9/estructurarepetitivawhile/EstructuraRepetitivaWhile.java
@@ -0,0 +1,18 @@
+package pkg9.estructurarepetitivawhile;
+
+/**
+ *
+ * @author diego.diaz
+ */
+public class EstructuraRepetitivaWhile {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while(i <= 10){
+            System.out.print(i + ", ");
+            i+=2;
+        }
+        
+    }
+    
+}
