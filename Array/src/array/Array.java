e9a77899b8799925c8bcb49ad4ddcff27370f1c5
Diego Diaz <diego.diaz@desarrollo.ddbcol.com.co>
2019-10-02 14:15:00 -0500

    Ejercicio basico con arreglos, asignar valores e imprimir dichos valores

---
+++ Array/src/array/Array.java
@@ -0,0 +1,26 @@
+package array;
+
+/**
+ *
+ * @author diego.diaz
+ */
+public class Array {
+
+    public static void main(String[] args) {
+        int edades[] = new int[5];
+        
+        edades[0] = 34;
+        edades[1] = 10;
+        edades[2] = 54;
+        edades[3] = 4;
+        edades[4] = 84;
+        
+        System.out.println("["+edades[0]+"]");
+        System.out.println("["+edades[1]+"]");
+        System.out.println("["+edades[2]+"]");
+        System.out.println("["+edades[3]+"]");
+        System.out.println("["+edades[4]+"]");
+        
+    }
+    
+}
