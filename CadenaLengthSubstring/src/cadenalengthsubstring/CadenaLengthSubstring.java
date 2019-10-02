c6c7e51f02edf71f2d44e85b75dc3fbdf52dc60f
Diego Diaz <diego.diaz@desarrollo.ddbcol.com.co>
2019-10-02 12:00:18 -0500

    Ejercicio de manejo de cadenas utilizando los metodos length() para contar la cantidad de caracteres y substring() para definir desde donde quiero mostrar mi cadena

---
+++ CadenaLengthSubstring/src/cadenalengthsubstring/CadenaLengthSubstring.java
@@ -0,0 +1,28 @@
+package cadenalengthsubstring;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author diego.diaz
+ */
+public class CadenaLengthSubstring {
+
+    public static void main(String[] args) {
+        String cadena;
+        int ncadena, desde, hasta;
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Ingrese una cadena de texto: ");
+        cadena = entrada.nextLine();
+        ncadena = cadena.length();
+        System.out.println(cadena + " tiene " + ncadena + " caracteres");
+        
+        System.out.print("¿Desde que parte deseas obtener la nueva? :");
+        desde = entrada.nextInt();
+        System.out.print("¿Hasta que parte deseas obtener la nueva? :");
+        hasta = entrada.nextInt();
+        
+        System.out.println("Nueva cadena: " + cadena.subSequence(desde, hasta));
+    }
+    
+}
