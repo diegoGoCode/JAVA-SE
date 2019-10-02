8de128ad3036164a6cf0fbc0006540cddae90296
Diego Diaz <diego.diaz@desarrollo.ddbcol.com.co>
2019-10-02 07:40:53 -0500

    Ejercicio estructura repetitiva for

---
+++ 8-EstructuraRepetitivaFor/src/pkg8/estructurarepetitivafor/EstructuraRepetitivaFor.java
@@ -0,0 +1,18 @@
+package pkg8.estructurarepetitivafor;
+
+/**
+ *
+ * @author diego.diaz
+ */
+public class EstructuraRepetitivaFor {
+
+    public static void main(String[] args) {
+       
+        for (int i = 1; i <= 5; i++) {
+            
+            System.out.print(i + ",");
+                
+        }
+    }
+    
+}
